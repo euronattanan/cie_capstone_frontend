16896192dcd1a79d70558b9e09f0b53e9781fd14
Euro Nattanan <53858594+euronattanan@users.noreply.github.com>
2022-03-09 22:12:59 +0700

    create a qr scanner program and link power bi with firebase

--- qrcode_reader_generator/use_qr_scanner/scanner.py
+++ qrcode_reader_generator/use_qr_scanner/scanner.py
@@ -1,6 +1,8 @@
 import pyrebase
 import json 
+from datetime import date, datetime 
 
+#firebase configuration variables
 firebaseConfig = {
   "apiKey": "AIzaSyCNrL7UCpZ4L-QGG9E_kjJQ_bxY1d4BGlw",
   "authDomain": "novid19-screening-system.firebaseapp.com",
@@ -11,18 +13,21 @@ firebaseConfig = {
   "appId": "1:310126395740:web:8597fa89e9847870204199"
 }
 
+#initializing firebase app and database
 firebase = pyrebase.initialize_app(firebaseConfig)
-
-text = '{"firstName":"Euro","lastName":"Euro","contact":"Euro","numberOfVaccines":2,"doses":["Pfizer","Pfizer"]}'
-dict = {"firstName":"Euro","lastName":"Euro","contact":"Euro","numberOfVaccines":2,"doses":["Pfizer","Pfizer"]}
-
-
 db = firebase.database()
 
 scanning = 1
 
 while scanning:
     qr_input = input("----Waiting for QR Code input-----\n")
+    date_now = str(date.today())
+    time_now = str(datetime.now().strftime("%H:%M:%S"))
+
+    if qr_input == "exit" or qr_input =="q":
+        print("Exiting program...")
+        break
+
     json_data = json.loads(qr_input)
     user_data = {
         "firstname": json_data["firstName"],
@@ -31,7 +36,11 @@ while scanning:
         "vaccineNum": json_data["numberOfVaccines"],
         "firstDose": json_data["doses"][0],
         "secondDose": json_data["doses"][1],
+        "thirdDose": "none",
+        "date": date_now,
+        "time": time_now
     }
+
     if len(json_data["doses"]) > 2:
         user_data["thirdDose"] = json_data["doses"][2]
 
---
+++ qrcode_reader_generator/use_qr_scanner/test.py
@@ -0,0 +1,5 @@
+import time
+from datetime import date, datetime 
+
+print(date.today())
+print(datetime.now().strftime("%H:%M:%S"))
