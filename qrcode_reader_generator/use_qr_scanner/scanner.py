0c9ec81b0ba82f83ad8b96da647ab2ef60103ebc
Euro Nattanan <53858594+euronattanan@users.noreply.github.com>
2022-05-24 21:12:16 +0700

    Update scanner.py

--- qrcode_reader_generator/use_qr_scanner/scanner.py
+++ qrcode_reader_generator/use_qr_scanner/scanner.py
@@ -4,13 +4,7 @@ from datetime import date, datetime
 
 #firebase configuration variables
 firebaseConfig = {
-  "apiKey": "AIzaSyCNrL7UCpZ4L-QGG9E_kjJQ_bxY1d4BGlw",
-  "authDomain": "novid19-screening-system.firebaseapp.com",
-  "databaseURL": "https://novid19-screening-system-default-rtdb.firebaseio.com",
-  "projectId": "novid19-screening-system",
-  "storageBucket": "novid19-screening-system.appspot.com",
-  "messagingSenderId": "310126395740",
-  "appId": "1:310126395740:web:8597fa89e9847870204199"
+  #firebase config parameters
 }
 
 #initializing firebase app and database
